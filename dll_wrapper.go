c8ef2988b3356acbda618ff80f9dc9d1b3f762a7
sergwin <sergwin@WIN-116894EDUKQ>
2026-03-17 02:09:23 +0700

    interacted with tui

--- dll_wrapper.go
+++ dll_wrapper.go
@@ -3,8 +3,17 @@
 
 package main
 
+/*
+#include <stdint.h>
+
+typedef struct {
+    int32_t success;
+    char data[256];
+    int32_t error_code;
+} Result;
+*/
+import "C"
 import (
-	"C"
 	"VirtualMemoryManagement/api"
 	"VirtualMemoryManagement/types/result"
 )
