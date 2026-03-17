14acd7ee32ead490638c84e467172f95c2199e27
mrred <zarubinr85@gmail.com>
2026-03-17 17:55:06 +0700

    +Add Result struct

--- CLI/TimpLaba2_VirtualMemory/TimpLaba2_VirtualMemory/Models/VirtualMemoryMock.cs
+++ CLI/TimpLaba2_VirtualMemory/TimpLaba2_VirtualMemory/Models/VirtualMemoryMock.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace TimpLaba2_VirtualMemory.Models
 {
     public class VirtualMemoryMock : IVirtualMemmoryFileWorker
     {
-        public void CloseFile()
-        {
-            Console.WriteLine("CloseFile");
-        }
-
         public void CreateFile(string fileName, VMFileType valueType)
         {
             Console.WriteLine("CreateFile");
+        }
+
+        public void CloseFile()
+        {
+            Console.WriteLine("CloseFile");
         }
 
         public IVirtualMemmoryValueWorker OpenFile(string fileName)
@@ -41,6 +42,30 @@ namespace TimpLaba2_VirtualMemory.Models
         public void WriteValue(int index, string value)
         {
             Console.WriteLine("WriteValue");
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Result
+    {
+        public int success;
+
+        public int error_code;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
+        public byte[] data;
+    }
+
+    public static class ResultExtensions
+    {
+        public static bool IsSuccess(this Result result) => result.success != 0;
+
+        public static string GetDataAsString(this Result result)
+        {
+            if (result.data == null) return string.Empty;
+            int length = Array.IndexOf(result.data, (byte)0);
+            if (length < 0) length = result.data.Length;
+            return System.Text.Encoding.UTF8.GetString(result.data, 0, length);
         }
     }
 }
