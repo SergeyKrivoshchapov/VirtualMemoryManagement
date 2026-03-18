e9ed01805868b930c169aeae517c06bb819a899d
mrrred <zarubinr85@gmail.com>
2026-03-18 16:17:46 +0700

    without nuget((((

---
+++ CLI/TimpLaba2_VirtualMemory/TerminalCommandMenu/Abstractions/ICommandParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TerminalCommandMenu.Abstractions
+{
+    public interface ICommandParser
+    {
+        (string Command, string arguments) Parse(string input);
+    }
+
+    public interface ICommandsParser
+    {
+        List<(string Command, string arguments)> Parse(string input);
+    }
+}
---
+++ CLI/TimpLaba2_VirtualMemory/TerminalCommandMenu/Abstractions/ITerminal.cs
@@ -0,0 +1,14 @@
+﻿namespace TerminalCommandMenu.Abstractions
+{
+    public interface IWriteble
+    {
+        void Write(string text);
+    }
+
+    public interface IReadeble
+    {
+        string? Read();
+    }
+
+    public interface ITerminal : IWriteble, IReadeble { }
+}
